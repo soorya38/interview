46eaa656e88c6bc39263c2c82895387cbe5506e1
soorya <soorya@abovecloud9.ai>
2025-03-01 11:50:58 +0530

    initial backend commit

---
+++ backend/services/user-service/go.mod
@@ -0,0 +1,3 @@
+module github.com/user/user-service
+
+go 1.19 
--- front-end/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import QuestionView from './components/QuestionView';
-
-function App() {
-  return <QuestionView />;
-}
-
-export default App;
